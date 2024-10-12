ddee352bde71f3815dea4f03491f34e17cc1882d
mahendi-hasan-mackbook-2020 <mehandimarediya92@gmail.com>
2024-10-12 13:55:38 +0530

    Fix ui changes

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import axios from 'axios';
-axios.defaults.baseURL =
-  Platform.OS === 'android' ? 'http://10.0.2.2:8080' : 'http://localhost:3000';
+axios.defaults.baseURL ='http://localhost:3000';
 AppRegistry.registerComponent(appName, () => App);
