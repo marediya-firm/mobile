f3518b81b29158e3f3ac8b801e980a6b55559618
Mahendihasan Marediya <mahendihasan.marediy@administrators-MacBook-Pro-5.local>
2024-10-23 18:56:11 +0530

    string constant

--- src/screen/dashboard/History/History.tsx
+++ src/screen/dashboard/History/History.tsx
@@ -1,18 +1,19 @@
-import { ImageBackground, Text, View } from 'react-native';
+import { ImageBackground, View } from 'react-native';
 import React from 'react';
-import { CustomView } from '../../../components/CoreComponent';
-import { fontStyleVariant, variant } from '../../../utils';
+import { CustomText, CustomView } from '../../../components/CoreComponent';
+import { variant } from '../../../utils';
 import { MenuIcon } from '../../../assets/icon';
 import { appImages } from '../../../assets/image';
 import { Calendar } from './components/CalenderRecordView';
 import { useMakeStyle } from './styles';
+import { strings } from '../../../constant';
 
 // export const punchRecord: PunchRecord[] = [
 //   {
 //     day: moment(),
 //     punchIn: '09:08 AM',
 //     punchOut: '06:05 PM',
-//     totalHours: '08:13 ',
+//     totalHours: '08:13',
 //   },
 // ];
 
@@ -22,9 +23,11 @@ const History = () => {
     <ImageBackground style={styles.wrapper} source={appImages.background}>
       <CustomView style={styles.wrapper}>
         <View style={styles.headerText}>
-          <Text style={[fontStyleVariant[variant.F50022], styles.headerText]}>
-            Attendance History
-          </Text>
+          <CustomText
+            text={strings.attendanceHistory}
+            variant={variant.F50022}
+            extraStyle={styles.headerText}
+          />
           <MenuIcon />
         </View>
         <View style={styles.calenderView}>
