097fbdbf0ed752ed4ce4f99e4bf1344e4cfbb1a4
mahendi-hasan-IG <mahendihasan.marediya@impactguru.com>
2024-11-04 13:12:36 +0530

    added test cases setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverageFrom: ['**/*.{ts,tsx}'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['html', 'json-summary', 'text', 'text-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+    './src/utils': {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-background-actions)/)',
+  ],
 };
